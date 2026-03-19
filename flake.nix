e8f0c790e05428bfb42264c48ec297a4cf7770ae
pwnwriter <hi@pwnwriter.me>
2026-03-19 16:48:50 -0400

    init(skel): skeleton cli, engine for seg

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems =
+        function: nixpkgs.lib.genAttrs systems (system: function (import nixpkgs { inherit system; }));
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+
+          packages = with pkgs; [
+          ];
+
+          buildInputs = with pkgs; [ pkg-config ];
+
+          shellHook = ''
+            echo "DevShell🚀: initiated"
+          '';
+        };
+      });
+    };
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+pub mod cli;
+pub mod engine;
+pub mod report;
+
+fn main() {
+    println!("Hello, world!");
+}
